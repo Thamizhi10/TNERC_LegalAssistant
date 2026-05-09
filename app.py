7dde5abce2f6de58845d2374dda48ca230ae9c37
Thamizhi S I <thamizhisi@Thamizhis-MacBook-Air.local>
2026-05-09 11:00:06 +0530

    removing token limit

--- app.py
+++ app.py
@@ -243,7 +243,7 @@ if st.session_state.data_loaded:
     Document Content:
     {file_text}
     """
-    query = combined_text
+    query = combined_text#[:6000]
     if not query.strip():
         st.warning("Please upload a file or enter case details")
         st.stop()
