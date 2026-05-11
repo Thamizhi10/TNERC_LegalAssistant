472577167e68a65692662d10bc8a065099663e9b
Thamizhi S I <thamizhisi@Thamizhis-MacBook-Air.local>
2026-05-11 19:55:54 +0530

    removing duplicate chunk load

--- app.py
+++ app.py
@@ -247,7 +247,8 @@ if not st.session_state.data_loaded:
 # ---------- MAIN APP ----------
 if st.session_state.data_loaded:
 
-    rulings_chunks, reg_chunks = load_chunks()
+    #rulings_chunks, reg_chunks = load_chunks()
+    st.write("App Loaded")
 
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
